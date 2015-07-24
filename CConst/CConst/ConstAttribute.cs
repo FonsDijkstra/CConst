0b87b7123410f712ff56a8538680992dfcec6e57
FonsDijkstra <fons.dijkstra@gmail.com>
2015-07-24 18:59:46 +0200

    Mark the ConstAttribute as not inherited

--- CConst/CConst/ConstAttribute.cs
+++ CConst/CConst/ConstAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace FonsDijkstra.CConst
 {
-    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public sealed class ConstAttribute : Attribute
     {
     }
