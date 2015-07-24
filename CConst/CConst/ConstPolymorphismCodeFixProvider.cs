5a0f361edb0040a78c75b9237f8caa697adebd70
FonsDijkstra <fons.dijkstra@gmail.com>
2015-07-24 18:59:06 +0200

    Provide code fixer for the CConst5 diagnostic

---
+++ CConst/CConst/ConstPolymorphismCodeFixProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Composition;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace FonsDijkstra.CConst
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ConstPolymorphismCodeFixProvider)), Shared]
+    public class ConstPolymorphismCodeFixProvider : CodeFixProvider
+    {
+        public const string DiagnosticId = ConstPolymorphismAnalyzer.DiagnosticId;
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+            var method = root.FindNode(context.Span) as MethodDeclarationSyntax;
+            if (method != null)
+            {
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        "Add const declaration",
+                        c => method.AddConstAttributeAsync(context.Document, c),
+                        DiagnosticId + "_add"),
+                    context.Diagnostics.Single());
+            }
+        }
+    }
+}
