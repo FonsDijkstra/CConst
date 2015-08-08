3a3a0d534fe47475878c5b30cbbd8cf2ebe077f9
Fons Dijkstra <fons.dijkstra@gmail.com>
2015-08-08 20:39:49 +0200

    File for syntax constructions to visualize

---
+++ CConst/CConst.Test/SyntaxVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FonsDijkstra.CConst.Test
+{
+    class SyntaxVisualizer
+    {
+        private int i;
+
+        int J { get; set; }
+
+        public SyntaxVisualizer()
+        {
+            i = 0;
+            J = 0;
+        }
+    }
+}
