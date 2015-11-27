64b3a7999ad712419c452903c1247958c02c35f1
Fons Dijkstra <fons.dijkstra@gmail.com>
2015-11-27 10:42:52 +0100

    Extra test code

--- CConst/CConst.Test/SyntaxVisualizer.cs
+++ CConst/CConst.Test/SyntaxVisualizer.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace FonsDijkstra.CConst.Test
 {
+    class Iets
+    {
+        public int a;
+    }
+
     class SyntaxVisualizer
     {
         private int i;
@@ -14,8 +19,13 @@ namespace FonsDijkstra.CConst.Test
 
         public SyntaxVisualizer()
         {
+            int k;
+            k = 1;
+
             i = 0;
             J = 0;
+
+            i = new Iets { a = 1, }.a;
         }
     }
 }
